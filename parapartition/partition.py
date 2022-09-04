90ebdfb8d16535d0cb0081a84d2d4ce6b581216a
luise koehler <kunstloses_brot@gmx.de>
2022-09-04 18:27:41 +0200

    Implement splitting plain text files

--- parapartition/partition.py
+++ parapartition/partition.py
@@ -9,7 +9,24 @@ logger = logging.getLogger(__name__)
 def split_into_paragraphs(
     file_path: str, format: str = None
 ) -> Generator[Tuple[str, int, str], None, None]:
-    pass
+    format = format if format is not None else detect_format(file_path)
+    if format is None:
+        return
+    if format == "plain":
+        yield from _split_plain_text(file_path)
+    elif format == "xml":
+        yield from _split_xml(file_path)
+    elif format == "html":
+        yield from _split_html(file_path)
+    elif format == "tei":
+        yield from _split_tei(file_path)
+    else:
+        logger.warning("No valid format, skipping %s " % file_path)
+        return
+
+
+# raw text
+# xml /tei : list, table, paragraph , head to paragraph
 
 
 def detect_format(file_path):
@@ -20,3 +37,21 @@ def detect_format(file_path):
         )
         return None
     return format[1]
+
+
+def _split_tei(file_path: str) -> Generator[Tuple[str, int, str], None, None]:
+    pass
+
+
+def _split_html(file_path: str) -> Generator[Tuple[str, int, str], None, None]:
+    pass
+
+
+def _split_xml(file_path: str) -> Generator[Tuple[str, int, str], None, None]:
+    pass
+
+
+def _split_plain_text(file_path: str) -> Generator[Tuple[str, int, str], None, None]:
+    with open(file_path, "r", encoding="utf-8") as ptr:
+        for i, line in enumerate(ptr, 1):
+            yield (file_path, i, line.rstrip("\n"))
--- tests/test_partition.py
+++ tests/test_partition.py
@@ -23,13 +23,82 @@ class ParapartitionTester(unittest.TestCase):
     def test_detect_format_on_html_file(self):
         for file in self.files("html"):
             result = detect_format(file)
-            with self.subTest(file):
+            with self.subTest():
                 self.assertEqual(result, "html")
 
     def test_detect_format_for_empty_file(self):
         file = os.path.join(self.testdata, "empty.txt")
         result = detect_format(file)
         self.assertIsNone(result)
+
+    def test_split_raw_text_file_all_lines_returned_individually(self):
+        file = os.path.join(self.testdata, "raw", "raw.txt")
+        result = list(split_into_paragraphs(file))
+        self.assertEqual(len(result), 17)
+
+    def test_paragraph_content_returned(self):
+        file = os.path.join(self.testdata, "raw", "raw.txt")
+        result = [paragraph[2] for paragraph in split_into_paragraphs(file)]
+        with open(file, encoding="utf-8") as ptr:
+            expected = [line.rstrip("\n") for line in ptr]
+        self.assertEqual(result, expected)
+
+    def test_split_raw_text_file_lines_enumerated_correctly(self):
+        file = os.path.join(self.testdata, "raw", "raw.txt")
+        result = [paragraph[1] for paragraph in split_into_paragraphs(file)]
+        self.assertEqual(result, list(range(1, 18)))
+
+    def test_split_file_first_value_always_file_name_raw(self):
+        for file in self.files("raw"):
+            result = {
+                paragraph[0]
+                for paragraph in split_into_paragraphs(file, format="plain")
+            }
+            with self.subTest():
+                self.assertEqual(result, {file})
+
+    def test_split_file_first_value_always_file_name_xml(self):
+        for file in self.files("xml"):
+            result = {paragraph[0] for paragraph in split_into_paragraphs(file, "xml")}
+            with self.subTest():
+                self.assertEqual(result, {file})
+
+    def test_split_file_first_value_always_file_name_html(self):
+        for file in self.files("html"):
+            result = {paragraph[0] for paragraph in split_into_paragraphs(file, "html")}
+            with self.subTest():
+                self.assertEqual(result, {file})
+
+    def test_split_xml_into_paragraphs_source_line_numbers_returned_correctly(self):
+        pass
+
+    def test_split_xml_text_content_returned_correctly(self):
+        pass
+
+    def test_split_html_source_line_numbers_returned_correctly(self):
+        pass
+
+    def test_split_html_text_content_returned_correctly(self):
+        pass
+
+    def test_split_tei_header_ignored(self):
+        pass
+
+    def test_split_tei_sources_lines_numbered_correctly(self):
+        pass
+
+    def test_split_tei_text_content_returned(self):
+        pass
+
+    def test_split_empty_file_into_paragraphs(self):
+        file = os.path.join(self.testdata, "empty.txt")
+        result = list(split_into_paragraphs(file))
+        self.assertEqual(result, [])
+
+    def test_invalid_format_returns_empty_list(self):
+        file = "some_file"
+        result = list(split_into_paragraphs(file, "some_format"))
+        self.assertEqual(result, [])
 
     def files(self, format):
         testfiles = {

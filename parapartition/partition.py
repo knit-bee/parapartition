a54afcb29bb3f6fd17eae59d384742fc80d6db51
luise koehler <kunstloses_brot@gmx.de>
2022-09-16 13:57:23 +0200

    Ignore teiHeader

--- parapartition/partition.py
+++ parapartition/partition.py
@@ -34,7 +34,8 @@ def _split_tei(file_path: str) -> Generator[Tuple[str, int, str], None, None]:
     other_tags = {"table", "list"}
     tags_to_iter = {f"{{*}}{tag}" for tag in p_like_tags.union(other_tags)}
     document = etree.parse(file_path)
-    for element in document.iter(tags_to_iter):
+    text_root = _determine_text_beginnig(document)
+    for element in text_root.iter(tags_to_iter):
         el_tag = etree.QName(element.tag).localname
         if el_tag in p_like_tags:
             # ignore <p> that are children of list or table
@@ -59,6 +60,15 @@ def _split_plain_text(file_path: str) -> Generator[Tuple[str, int, str], None, N
     with open(file_path, "r", encoding="utf-8") as ptr:
         for i, line in enumerate(ptr, 1):
             yield (file_path, i, line.rstrip("\n"))
+
+
+def _determine_text_beginnig(xml_tree: etree._ElementTree) -> etree._Element:
+    tei_header = xml_tree.find(".//{*}teiHeader")
+    if tei_header is None:
+        text_root = xml_tree
+    else:
+        text_root = tei_header.getnext()
+    return text_root
 
 
 def _gather_complex_element_text(element: etree._Element) -> str:
